b2daf71a033ee60e758f3f1c938f3a4b48868736
Bogdan <popovich-77528@mail.ru>
2014-01-13 11:11:33 -0800

    Update Objednanuj kod.cpp

--- Objednanuj kod.cpp
+++ Objednanuj kod.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include <gl/glut.h>
+#include <glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <cmath>
@@ -8,13 +8,13 @@
 float WinWid=2000.0;
 float WinHei=1000.0;
 
-void drawCircle(float x, float y, float r, int amountSegments)
-{
- glBegin(GL_LINE_LOOP);
+void drawCircle(float x, float y, float r, int amountSegments) 
+{ 
+ glBegin(GL_LINE_LOOP); 
 
- for(int i = 0; i < amountSegments; i++)
- {
-  float angle = 2.0 * 3.1415926 * float(i) / float(amountSegments);
+ for(int i = 0; i < amountSegments; i++) 
+ { 
+  float angle = 2.0 * 3.1415926 * float(i) / float(amountSegments); 
 
   float dx = r * cosf(angle);
   float dy = r * sinf(angle);
@@ -22,11 +22,11 @@ void drawCircle(float x, float y, float r, int amountSegments)
   glVertex2f(x + dx, y + dy);
 }
 
- glEnd();
+ glEnd(); 
 }
 void Draw()
 {
-     glClear(GL_COLOR_BUFFER_BIT);
+         glClear(GL_COLOR_BUFFER_BIT);
          glColor3f(0.0, 0.0, 0.0);
          //Opora
          glBegin(GL_QUADS);
@@ -39,7 +39,7 @@ void Draw()
              glColor3f(0.0, 1.0, 0.0);
              glVertex2i(680, 250);
 
-          glEnd();
+         glEnd();
          //perekladina
          glBegin(GL_LINE_LOOP);
               
@@ -68,43 +68,42 @@ void Draw()
              glVertex2i(680, 250);
                          
          glEnd();
-         //kulka
-         glColor3f(1.0, 0.0, 0.0);
-         drawCircle(689, 321, 50,60);
-     
-        glBegin(GL_LINE_LOOP); //Romb
-               
+
+        //kulka
+             glColor3f(1.0, 0.0, 0.0);
+             drawCircle(689, 321, 50,60);
+
+        //Romb 
+	    glBegin(GL_LINE_LOOP);  
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-100, -325);
+              glVertex2i(-100, -460);
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-175, -250);
+              glVertex2i(-175, -385);
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-100, -175);
+              glVertex2i(-100, -310);
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-25, -250);
- 
+              glVertex2i(-25, -385);
         glEnd();
 
-                
-
-        glBegin(GL_LINE_STRIP); //Garmata
+        //Garmata
+        glBegin(GL_LINE_STRIP);                     
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-770, -275);
+              glVertex2i(-770, -400);
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-500, -275);
+              glVertex2i(-500, -400);
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-500, -247);
+              glVertex2i(-500, -372);
               glColor3f(0.0, 0.0, 0.0);
-              glVertex2i(-770, -247);
+              glVertex2i(-770, -372);
          glEnd();
 
-          
-                 glColor3f(0.0, 0.0, 0.0); //koleso vid pushki
-         drawCircle(-700, -307, 30,60);
-      
-                 //Pidloga
-         glBegin(GL_LINE_LOOP);
-         glColor3f(0.0, 0.0, 0.0);
+         //koleso vid pushki 
+		 glColor3f(0.0, 0.0, 0.0);                     
+         drawCircle(-700, -430, 30,60);
+
+		 //Pidloga
+		 glBegin(GL_LINE_LOOP);
+		      glColor3f(0.0, 0.0, 0.0);
               glVertex2i(-999, -460);
               glColor3f(0.0, 0.0, 0.0);
               glVertex2i(-999, -499);
@@ -113,9 +112,9 @@ void Draw()
               glColor3f(0.0, 0.0, 0.0);
               glVertex2i(999,-460 );
          glEnd();
-                 //lovyshka 1
-         glBegin(GL_QUADS);
-         glColor3f(1.0, 0.0, 0.0);
+		 //lovyshka 1
+		 glBegin(GL_QUADS);
+		      glColor3f(1.0, 0.0, 0.0);
               glVertex2i(445, -200);
               glColor3f(1.0, 0.0, 0.0);
               glVertex2i(445, -460);
@@ -124,9 +123,9 @@ void Draw()
               glColor3f(1.0, 0.0, 0.0);
               glVertex2i(480,-200 );
          glEnd();
-                 //lovyshka 2
-         glBegin(GL_QUADS);
-         glColor3f(1.0, 0.0, 0.0);
+		 //lovyshka 2
+		 glBegin(GL_QUADS);
+		      glColor3f(1.0, 0.0, 0.0);
               glVertex2i(195, -200);
               glColor3f(1.0, 0.0, 0.0);
               glVertex2i(195, -460);
@@ -135,13 +134,37 @@ void Draw()
               glColor3f(1.0, 0.0, 0.0);
               glVertex2i(230,-200 );
          glEnd();
-         glBegin(GL_LINE_LOOP);
-                glVertex2i(335, -400);
-                glVertex2i(295,-460);
-                glVertex2i(375,-460);
-         glEnd();
-                
-                 glFlush();                
+		 glBegin(GL_LINE_LOOP);
+                glVertex2i(335, -400);  
+                glVertex2i(295,-460);  
+                glVertex2i(375,-460);  
+        glEnd();
+		glBegin(GL_LINE_STRIP); //каряве півколо
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-770, -400);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-775, -398);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-779, -395);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-782, -391);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-784, -386);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-782, -381);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-779, -377);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-775, -374);
+glColor3f(0.0, 0.0, 0.0);
+glVertex2i(-770, -372);
+glEnd();
+
+
+      
+		 
+
+		  glFlush();		     
 }
 
 void Initialize()
@@ -156,7 +179,7 @@ void Initialize()
 int main(int argc, char** argv)
 {
         //initialization
-        glutInit(&argc, argv);
+        glutInit(&argc,        argv);         
         glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
         glutInitWindowSize(WinWid, WinHei);
         glutInitWindowPosition(0,0);
