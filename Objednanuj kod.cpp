246f5c4bf16dbd7c4d6394cac85945e89d18a95d
Andriy1 <Shtuka-andriy@mail.ru>
2014-01-27 21:13:58 +0200

    Revert "0"
    This reverts commit 14eeb27c6468c5775c40a3ed29605d71d9509880.

--- Objednanuj kod.cpp
+++ Objednanuj kod.cpp
@@ -1,77 +1,193 @@
-#include<gl\glut.h>
-#include<math.h>
-#include<stdlib.h>
+#include "stdafx.h"
+#include <glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <cmath>
 
-float WinWid=400.0;
-float WinHei=400.0;
-float Angle=0.0, Scale=1.0;
 
+float WinWid=2000.0;
+float WinHei=1000.0;
+
+void drawCircle(float x, float y, float r, int amountSegments) 
+{ 
+ glBegin(GL_LINE_LOOP); 
+
+ for(int i = 0; i < amountSegments; i++) 
+ { 
+  float angle = 2.0 * 3.1415926 * float(i) / float(amountSegments); 
+
+  float dx = r * cosf(angle);
+  float dy = r * sinf(angle);
+
+  glVertex2f(x + dx, y + dy);
+}
+
+ glEnd(); 
+}
 void Draw()
 {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glPushMatrix();
-        Scale=1.0-abs(sin(3.14*Angle/90.0)/sqrt(3.0)/2);
-        glRotatef(Angle, 0.0, 0.0, 1.0);
-        glScalef(Scale, Scale, 1.0);
-        glBegin(GL_LINES);
-                for(float i=-WinWid/2; i<=WinWid/2; i+=20.0)
-                {
-                        glVertex2f(i, -WinHei/2);
-                        glVertex2f(i, WinHei/2);
-                }
-                for(float i=-WinHei/2; i<=WinHei/2; i+=20.0)
-                {
-                        glVertex2f(-WinWid/2, i);
-                        glVertex2f(WinWid/2, i);
-                }
+         glClear(GL_COLOR_BUFFER_BIT);
+         glColor3f(0.0, 0.0, 0.0);
+         //Opora
+         glBegin(GL_QUADS);
+             glColor3f(0.0, 1.0, 0.0);
+             glVertex2i(680, -460);
+             glColor3f(0.0, 1.0, 0.0);
+             glVertex2i(700, -460);
+             glColor3f(0.0, 1.0, 0.0);
+             glVertex2i(700, 250);
+             glColor3f(0.0, 1.0, 0.0);
+             glVertex2i(680, 250);
+
+         glEnd();
+         //perekladina
+         glBegin(GL_LINE_LOOP);
+              
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(500, 250);
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(880, 250);
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(880, 270);
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(500, 270);
+
+         glEnd();
+         //punktir
+         //glEnable(GL_LINE_STIPPLE);
+         //glLineStipple(1,0x00FF);
+         glBegin(GL_LINE_LOOP);
+                 
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(680, -460);
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(700, -460);
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(700, 250);
+             glColor3f(0.0, 0.0, 0.0);
+             glVertex2i(680, 250);
+                         
+         glEnd();
+
+        //kulka
+             glColor3f(1.0, 0.0, 0.0);
+             drawCircle(689, 321, 50,60);
+
+        //Romb 
+	    glBegin(GL_LINE_LOOP);  
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-100, -460);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-175, -385);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-100, -310);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-25, -385);
         glEnd();
-        glPopMatrix();
-        glutSwapBuffers();
-}
 
-void Timer(int value)
-{
-        switch(value)
-        {
-        case 0: glColor3f(1.0, 1.0, 1.0);
-                break;
-        case 1: glColor3f(1.0, 0.0, 0.0);
-                break;
-        }
-        glColor3f(1.0, 1.0, 1.0);
-        ++Angle;
-        //Draw();
-        glutPostRedisplay();
-        glutTimerFunc(50, Timer, rand() % 2);
-}
+        //Garmata
+        glBegin(GL_LINE_STRIP);                     
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-770, -400);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-500, -400);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-500, -372);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-770, -372);
+         glEnd();
 
-void Timer2(int)
-{
-        glColor3f(0.0, 1.0, 0.0);
-        glutPostRedisplay();
-        glutTimerFunc(1000, Timer2, 0);
+         //koleso vid pushki 
+		 glColor3f(0.0, 0.0, 0.0);                     
+         drawCircle(-700, -430, 30,60);
+
+		 //Pidloga
+		 glBegin(GL_LINE_LOOP);
+		      glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-999, -460);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(-999, -499);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(999, -499);
+              glColor3f(0.0, 0.0, 0.0);
+              glVertex2i(999,-460 );
+         glEnd();
+		 //lovyshka 1
+		 glBegin(GL_QUADS);
+		      glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(445, -200);
+              glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(445, -460);
+              glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(480, -460);
+              glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(480,-200 );
+         glEnd();
+		 //lovyshka 2
+		 glBegin(GL_QUADS);
+		      glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(195, -200);
+              glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(195, -460);
+              glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(230, -460);
+              glColor3f(1.0, 0.0, 0.0);
+              glVertex2i(230,-200 );
+         glEnd();
+		 glBegin(GL_LINE_LOOP);
+                glVertex2i(335, -400);  
+                glVertex2i(295,-460);  
+                glVertex2i(375,-460);  
+        glEnd();
+        glBegin(GL_LINE_STRIP); //"kariave" pivkolo
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-770, -400);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-775, -398);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-779, -395);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-782, -391);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-784, -386);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-782, -381);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-779, -377);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-775, -374);
+               glColor3f(0.0, 0.0, 0.0);
+               glVertex2i(-770, -372);
+        glEnd();
+
+
+      
+		 
+
+glFlush();		     
 }
 
 void Initialize()
 {
-        glClearColor(0.0, 0.0, 0.0, 1.0);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-WinWid/2, WinWid/2, -WinHei/2, WinHei/2, -200.0, 200.0);
-        glMatrixMode(GL_MODELVIEW);
+     glClearColor(1.0, 1.0, 1.0, 1.0);
+         glMatrixMode(GL_PROJECTION);
+         glLoadIdentity();
+         glOrtho(-WinWid/2, WinWid/2, -WinHei/2, WinHei/2, -200.0, 200.0);
 }
+
 
 int main(int argc, char** argv)
 {
-        glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+        //initialization
+        glutInit(&argc,        argv);         
+        glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
         glutInitWindowSize(WinWid, WinHei);
-        glutInitWindowPosition(100, 200);
-        glutCreateWindow("Lesson 03");
-        glutDisplayFunc(Draw);
-        glutTimerFunc(50, Timer, 0);
-        glutTimerFunc(1000, Timer2, 0);
+        glutInitWindowPosition(0,0);
+        glutCreateWindow("1ZAVDANNYA");
+        //registration
+        glutDisplayFunc(Draw);// painting
         Initialize();
         glutMainLoop();
+        
         return 0;
 }
